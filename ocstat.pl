ee0863dcf78326ec96ac47a808f8351f118bf51d
ebillen <elmar.billen@gmail.com>
2015-01-07 11:44:06 +0100

    Perl script to download opencaching.de founds as csv-file

---
+++ ocstat.pl
@@ -0,0 +1,121 @@
+#!/usr/bin/perl -w
+
+use strict;
+use LWP::UserAgent;
+use JSON;
+
+# ===========================================
+# Hier Username bei opencaching.de eintragen:
+#
+my $oc_user = "hier_usernamen_eintragen";
+#
+# Hier den Consumer key eintragen:
+#
+my $oc_consumer_key = "hier_consumer_key_eintragen";
+#
+#
+my $separator = ", ";
+my $quote = "'";
+#
+#
+my $filename = "oc_stat.csv";
+#
+#
+# ===========================================
+
+my $okapi_url = "http://www.opencaching.de/okapi";
+
+my $fh;
+open($fh, ">", $filename) or die "Could not open $filename for writing!";
+
+# los geht's:
+my $ua = LWP::UserAgent->new();
+my $json = JSON->new->utf8();
+my $resp;
+
+my $uuid; # User ID at opencaching.de
+
+$resp = $ua->get($okapi_url . 
+		 "/services/users/by_username?" .
+		 "username=$oc_user&" .
+		 "fields=uuid&" .
+		 "consumer_key=$oc_consumer_key");
+
+if ($resp->is_success) {
+    my $data = $json->decode($resp->decoded_content);
+
+    $uuid = $data->{uuid};
+
+    print "Username: $oc_user\n";
+    print "User-ID:  $uuid\n";
+}
+else {
+    die $resp->status_line;
+}
+
+my $more = 1;
+my $limit = 10;
+my $offset = 0;
+
+while ($more) {
+    my $n_logs = 0;
+    
+    $resp = $ua->get($okapi_url . 
+		     "/services/logs/userlogs?" .
+		     "user_uuid=$uuid&" .
+		     "limit=$limit&" .
+		     "offset=$offset&" .
+		     "consumer_key=$oc_consumer_key");
+
+    if ($resp->is_success) {
+	my $data = $json->decode($resp->decoded_content);
+	my $occode = "";
+
+	foreach (@{$data}) {
+	    $n_logs += 1;
+	    my $log = $_;
+
+	    print $fh $quote . $log->{cache_code} . $quote . $separator;
+	    print $fh $quote . $log->{date} . $quote . $separator;
+	    print $fh $quote . $log->{type} . $quote . $separator;
+
+
+	    # $log enthaelt Cache-Code, Datum und Logtyp.
+	    # Name und Besitzer muessen wir noch holen:
+	
+	    $resp = $ua->get($okapi_url . 
+			     "/services/caches/geocache?" .
+			     "cache_code=" . $log->{cache_code} . "&" .
+			     "user_uuid=$uuid&" .
+			     "fields=name|owner&" .
+			     "consumer_key=$oc_consumer_key");
+	    if ($resp->is_success) {
+		my $data2 = $json->decode($resp->decoded_content);
+		print $fh $quote . $data2->{name} . $quote . $separator;
+		print $fh $quote . $data2->{owner}->{username} . $quote;
+	    }
+	    else {
+		die $resp->status_line;
+	    }  
+	    print $fh "\n";
+	}
+
+
+	if ($n_logs < $limit) {
+	    $more = 0;
+	}
+	else {
+	    $more = 1;
+	}
+
+	$offset = $offset + $limit;
+
+	print "Logs geladen: $offset...\n";
+    }
+    else {
+	die $resp->status_line;
+    }
+}
+
+close $fh;
+
